18333fa996ef0189c755a8e3181c7e9ae676008f
malghalary <anoyameen@gmail.com>
2023-02-28 20:06:12 -0500

    first commit

---
+++ Travel-App/src/client/index.js
@@ -0,0 +1,10 @@
+
+import {handleSubmit} from "./js/handleSubmit";
+import{planNow} from "./js/app.js";
+
+import './styles/style.scss'
+import './styles/header.scss'
+import './views/image/travel1.png'
+import './views/image/travel2.jpg'
+
+export{handleSubmit}
